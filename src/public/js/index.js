96d558b23469a173bd895e54b2692855678122c4
GonzaloGalindz <gonzagalin777@gmail.com>
2023-09-08 12:37:25 -0300

    working

--- src/public/js/index.js
+++ src/public/js/index.js
@@ -1,58 +1,7 @@
 const socketClient = io();
 
-const user = document.getElementById("correo");
-const formulario = document.getElementById("formulario");
-const inputMessage = document.getElementById("message");
-const divChat = document.getElementById("chat");
-
-let usuario;
-
-Swal.fire({
-  title: "BIENVENIDO",
-  text: "Ingresa tu correo electronico",
-  input: "text",
-  inputValidator: (value) => {
-    if (!value) {
-      return "Necesitas ingresar tu correo";
-    }
-  },
-}).then((correoDelUsuario) => {
-  usuario = correoDelUsuario.value;
-  user.innerText = `Hola ${usuario}`;
-  socketClient.emit("usuarioNuevo", usuario);
-});
-
-formulario.onsubmit = (e) => {
-  e.preventDefault();
-  const infoMensaje = {
-    correoDelUsuario: usuario,
-    message: inputMessage.value,
-  };
-  socketClient.emit("mensaje", infoMensaje);
-  inputMessage.value = "";
-};
-
-socketClient.on("chat", (messages) => {
-  const chat = messages
-    .map((objMessage) => {
-      return `<p>${objMessage.correoDelUsuario}: ${objMessage.message}</p>`;
-    })
-    .join(" ");
-  divChat.innerHTML = chat;
-});
-
-socketClient.on("broadcast", (usuario) => {
-  Toastify({
-    text: `${usuario} se ha conectado`,
-    duration: 5000,
-    style: {
-      background: "linear-gradient(to right, #00b09b, #96c93d)",
-    },
-  }).showToast();
-});
-
 const formProd = document.getElementById("formProduct");
-const title = document.getElementById("title");
+const name = document.getElementById("name");
 const description = document.getElementById("description");
 const price = document.getElementById("price");
 const stock = document.getElementById("stock");
@@ -64,18 +13,18 @@ const id = document.getElementById("id");
 formProd.onsubmit = (e) => {
   e.preventDefault();
   const objProd = {
-    title: title.value,
-    description: description.value,
+    name: name.value,
+    code: code.value,
     price: Number(price.value),
     stock: Number(stock.value),
-    code: code.value,
+    description: description.value,
   };
   socketClient.emit("agregar", objProd);
-  title.value = "";
-  description.value = "";
+  name.value = "";
+  code.value = "";
   price.value = "";
   stock.value = "";
-  code.value = "";
+  description.value = "";
 };
 
 formDelete.onsubmit = (e) => {
@@ -88,12 +37,11 @@ socketClient.on("added", (newProduct) => {
   if (typeof newProduct === "object") {
     const addRow = `
         <tr>
-            <td>${newProduct.id}</td>
-            <td>${newProduct.title}</td>
-            <td>${newProduct.description}</td>
+            <td>${newProduct.name}</td>
+            <td>${newProduct.code}</td>
             <td>${newProduct.price}</td>
             <td>${newProduct.stock}</td>
-            <td>${newProduct.code}</td>
+            <td>${newProduct.description}</td>
         </tr>`;
     tableProds.innerHTML += addRow;
   }
@@ -105,15 +53,65 @@ socketClient.on("deleted", (arrProducts) => {
       .map((objProd) => {
         return `
               <tr>
-                  <td>${objProd.id}</td>
-                  <td>${objProd.title}</td>
-                  <td>${objProd.description}</td>
+                  <td>${objProd.name}</td>
+                  <td>${objProd.code}</td>
                   <td>${objProd.price}</td>
                   <td>${objProd.stock}</td>
-                  <td>${objProd.code}</td>
+                  <td>${objProd.description}</td>
               </tr>`;
       })
       .join(" ");
     tableProds.innerHTML = addRow;
   }
 });
+
+// const user = document.getElementById("correo");
+// const formulario = document.getElementById("formulario");
+// const inputMessage = document.getElementById("message");
+// const divChat = document.getElementById("chat");
+
+// let usuario;
+
+// Swal.fire({
+//   title: "BIENVENIDO",
+//   text: "Ingresa tu correo electronico",
+//   input: "text",
+//   inputValidator: (value) => {
+//     if (!value) {
+//       return "Necesitas ingresar tu correo";
+//     }
+//   },
+// }).then((correoDelUsuario) => {
+//   usuario = correoDelUsuario.value;
+//   user.innerText = `Hola ${usuario}`;
+//   socketClient.emit("usuarioNuevo", usuario);
+// });
+
+// formulario.onsubmit = (e) => {
+//   e.preventDefault();
+//   const infoMensaje = {
+//     correoDelUsuario: usuario,
+//     message: inputMessage.value,
+//   };
+//   socketClient.emit("mensaje", infoMensaje);
+//   inputMessage.value = "";
+// };
+
+// socketClient.on("chat", (messages) => {
+//   const chat = messages
+//     .map((objMessage) => {
+//       return `<p>${objMessage.correoDelUsuario}: ${objMessage.message}</p>`;
+//     })
+//     .join(" ");
+//   divChat.innerHTML = chat;
+// });
+
+// socketClient.on("broadcast", (usuario) => {
+//   Toastify({
+//     text: `${usuario} se ha conectado`,
+//     duration: 5000,
+//     style: {
+//       background: "linear-gradient(to right, #00b09b, #96c93d)",
+//     },
+//   }).showToast();
+// });
