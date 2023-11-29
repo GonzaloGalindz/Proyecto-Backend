f8c9f31cb290b8430bd8440107f785499a623e60
Gonzalo Galindez <Gonzagalin777@gmail.com>
2023-11-28 22:20:17 -0300

    Errores de add product in cart, update product in cart y delete product in cart

--- src/routes/carts.router.js
+++ src/routes/carts.router.js
@@ -11,10 +11,13 @@ router.get("/:cid", cartsController.getCartById);
 
 router.delete("/:cid", cartsController.cartDelete);
 
+//Crea nuevos ids
 router.post("/:cid/products/:pid", cartsController.addProductInCart);
 
+//Me actualiza cantidad de producto y me lo crea con nuevo id
 router.put("/:cid/products/:pid", cartsController.updateProductInCart);
 
+//Me sale que lo borra correctamente, pero no lo borra y tiene un nuevo id
 router.delete("/:cid/products/:pid", cartsController.productDeleteInCart);
 
 export default router;
